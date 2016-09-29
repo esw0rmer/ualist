2fa6ad77464529a428f42ed94820fddb1a3fc7b5
esw0rmer <esw0rmer@gmail.com>
2016-09-29 14:26:33 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ualist',
+    version='0.1',
+    packages=[''],
+    url='www.whitehatz.org',
+    license='GPL',
+    author='esw0rmer',
+    author_email='esw0rmer@gmail.com',
+    description='User-Agents List'
+)
